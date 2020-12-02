f7d147c583624d07b73edd2c028d5d089ff69c4c
einomie <einomie@gmail.com>
2020-12-02 11:12:46 +0200

    db configs

--- config/config.js
+++ config/config.js
@@ -4,10 +4,10 @@ if (Deno.env.get('TEST_ENVIRONMENT')) {
   config.database = {};
 } else {
   config.database = {
-    hostname: "XXX.elephantsql.com",
-    database: "XXX",
-    user: "XXX",
-    password: "XXX",
+    hostname: "hattie.db.elephantsql.com",
+    database: "cbccxwqx",
+    user: "cbccxwqx",
+    password: "GUkQaUNWWIlmt4uEcMt_EAfc663DOJup",
     port: 5432
   };
 }
