09a073ceb5c140bd175c51748e5ab48803a83cb3
unknown <einomie@gmail.com>
2020-12-07 14:51:23 +0200

    summary of last 7 days + their averages

--- routes/controllers/helloController.js
+++ routes/controllers/helloController.js
@@ -2,18 +2,22 @@ import { getHello } from "../../services/helloService.js";
 import {postMorningData,getMorningSummary} from "../../services/mainService.js";
 
 const hello = ({render}) => {
-  render('index.ejs', { hello: getHello() });
+  render('index.ejs', { data: getHello() });
 };
 
 const postMorning = async({request,response}) => {
   const res = request.body()
   const data = await res.value
   console.log(data)
-  await postMorningData(data.get('sleepduration'),data.get('sleepquality'),data.get('mood'),2)    
+  await postMorningData(data.get('sleepduration'),data.get('sleepquality'),data.get('mood'),2)
+  location = '/morning'    
 }
 
 const morningSummary = async({render}) =>  {
-	render('index.ejs',{data:await getMorningSummary()});
+  const summary = await getMorningSummary()
+  const fullData = summary[0]
+  summary.shift()
+	render('summary.ejs',{data: fullData,averages: summary});
 }
 
 export { hello,postMorning,morningSummary };
--- services/mainService.js
+++ services/mainService.js
@@ -6,9 +6,49 @@ const postMorningData = async(sleepdur,sleepqua,mood,user_id) => {
 	
 }
 
+const calcAverage = (dat) => {
+	if(!dat || dat.length == 0 ){
+		return 0
+	}
+	return dat.reduce((a, b) => a + b, 0)/dat.length
+}
+
 const getMorningSummary = async() => {
-	const result = await executeQuery("SELECT * FROM data WHERE user_id = $1;",2);
-	return result.rowsOfObjects();
+	//query for last 7 days of data:
+	const result = await executeQuery("SELECT * FROM data WHERE user_id = $1 AND time BETWEEN NOW()::DATE-EXTRACT(DOW FROM NOW())::INTEGER-7 AND NOW()::DATE-EXTRACT(DOW from NOW())::INTEGER+2;",2);
+	const res = result.rowsOfObjects()
+	let avg_sleepdur = []
+	let avg_sleepquality = []
+	let avg_sportstime = []
+	let avg_mood = []
+	let avg_studytime = []
+	var i;
+	var z;
+	console.log(res)
+	for(i in res) {
+		console.log(res[i])
+		if(res[i].sleepduration) {
+			avg_sleepdur.push(Number(res[i].sleepduration))
+		}
+		if(res[i].sleepquality) {
+			avg_sleepquality.push(Number(res[i].sleepquality))
+		}
+		if(res[i].sportstime) {
+			avg_sportstime.push(Number(res[i].sportstime))
+		}
+		if(res[i].studytime) {
+			avg_studytime.push(Number(res[i].studytime))
+		}
+		if(res[i].mood) {
+			avg_mood.push(Number(res[i].mood))
+		}
+	}
+	avg_sleepquality = calcAverage(avg_sleepquality)
+	avg_sleepdur = calcAverage(avg_sleepdur)
+	avg_sportstime = calcAverage(avg_sportstime)
+	avg_studytime = calcAverage(avg_studytime)
+	avg_mood = calcAverage(avg_mood)
+	return [res,avg_sleepdur,avg_sleepquality,avg_sportstime,avg_studytime,avg_mood]
 }
 
 export { postMorningData,getMorningSummary } 
